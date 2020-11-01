4d5433b5a8e190b02e9967dc235733347404f87d
htchung <htchung@gms.tku.edu.tw>
2020-11-01 23:24:00 +0800

    Updates

---
+++ precache-manifest.b0a7dbfe45744e31ad382492e60fd785.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13454dca42f59273320b9022981f747b",
+    "url": "/1091-p1-classdemo/index.html"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/1091-p1-classdemo/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "2eba2e57708b5bf9dec6",
+    "url": "/1091-p1-classdemo/static/css/main.750e0d09.chunk.css"
+  },
+  {
+    "revision": "939c29a12718c22b5acf",
+    "url": "/1091-p1-classdemo/static/js/2.e7e24cc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/1091-p1-classdemo/static/js/2.e7e24cc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2eba2e57708b5bf9dec6",
+    "url": "/1091-p1-classdemo/static/js/main.e3024f34.chunk.js"
+  },
+  {
+    "revision": "673cf73d22c5946df5ea",
+    "url": "/1091-p1-classdemo/static/js/runtime-main.ec846226.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/1091-p1-classdemo/static/media/SEGA.82e09f78.TTF"
+  }
+]);
